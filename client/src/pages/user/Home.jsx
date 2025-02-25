aa8d2b7463a28f0dc0ab86dadf4c0a624eb06616
Sujan299 <mrsujan321@gmail.com>
2025-02-25 22:02:39 +0545

    added live link to readme

--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
     <div className="max-w-7xl mx-auto px-4 py-8">
       <div className="grid md:grid-cols-2 grid-cols-1 mt-16 justify-between gap-12">
         <div className="flex-1 bg-gray-200 rounded-xl overflow-hidden">
-          <img src={posts[3]?.image} alt="Featured Post" className="w-full h-full object-cover" />
+          <img src={posts[3]?.image} alt="Loading..." className="w-full h-full object-cover" />
         </div>
         <div className="flex-1 flex flex-col justify-around text-gray-700">
           <div className="opacity-65 my-1">{posts[3]?.date}</div>
@@ -62,7 +62,7 @@ const Home = () => {
         {posts.map((post) => (
           <Link key={post.id} to={`/blog/${post._id}`}>
             <div className="rounded-md bg-white shadow-lg overflow-hidden transform transition-all hover:scale-105 my-2">
-              <img src={post.image} className="rounded-t-lg h-48 w-full object-cover" alt={post.title} />
+              <img src={post.image} className="rounded-t-lg h-48 w-full object-cover" alt="Loading..." />
               <div className="px-4 py-2">
                 <div className="opacity-65 my-1">{post.date}</div>
                 <h1 className="text-xl font-semibold text-gray-800 my-2 h-40">{post.title}</h1>
